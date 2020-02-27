619043cba72b5c48e1149166b2d5aafd708f60c0
khoa <khoanguyen0791@gmail.com>
2020-02-26 23:14:49 -0600

    Some changes to log scale.

--- plot_csv.py
+++ plot_csv.py
@@ -1,18 +1,22 @@
-from typing import *
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from pandas import *
+from plotly.graph_objs._figure import *
 
-uvxy_df = pd.read_csv('UVXY.csv')
-vix_df = pd.read_csv('vixcurrent.csv')
+with open('vixcurrent.csv', 'r') as file_out:
+    with open('vixcurrent_fixed.csv', 'w') as file_in:
+        next(file_out)   # get rid of first line
+        for line in file_out:
+            file_in.write(line)
 
-print(type(uvxy_df))
+uvxy_df: DataFrame = pd.read_csv('UVXY.csv')
+vix_df: DataFrame = pd.read_csv('vixcurrent_fixed.csv')
 
-uvxy_fig = px.line(uvxy_df, x='Date', y='High', title='UVXY Price over time')
-uvxy_fig.update_layout(yaxis_type="log")
-vix_fig = px.line(vix_df, x='Date', y='VIX High', title='VIX number over time')
-
-print(type(uvxy_fig))
+uvxy_fig: Figure = go.Figure(go.Scatter(x=uvxy_df['Date'], y=uvxy_df['High'], name='high', mode='lines'))
+uvxy_fig.add_trace(go.Scatter(x=uvxy_df['Date'], y=uvxy_df['Close'], name='close', mode='lines'))
+uvxy_fig.update_layout(yaxis_type="log", title="UVXY price")
+vix_fig: line = px.line(vix_df, x='Date', y='VIX High', title='VIX number over time')
 
 uvxy_fig.show()
 vix_fig.show()
