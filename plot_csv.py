03d83723543c131220cdd265bdfa1ea66c82367f
khoa <khoanguyen0791@gmail.com>
2020-02-26 23:38:34 -0600

    Some changes to log scale.

--- plot_csv.py
+++ plot_csv.py
@@ -16,7 +16,9 @@ vix_df: DataFrame = pd.read_csv('vixcurrent_fixed.csv')
 uvxy_fig: Figure = go.Figure(go.Scatter(x=uvxy_df['Date'], y=uvxy_df['High'], name='high', mode='lines'))
 uvxy_fig.add_trace(go.Scatter(x=uvxy_df['Date'], y=uvxy_df['Close'], name='close', mode='lines'))
 uvxy_fig.update_layout(yaxis_type="log", title="UVXY price")
+
 vix_fig: line = px.line(vix_df, x='Date', y='VIX High', title='VIX number over time')
+vix_fig.update_layout(yaxis_type="log")
 
 uvxy_fig.show()
 vix_fig.show()
